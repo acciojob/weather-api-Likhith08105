78e8c603383e4cae45cac27188d7ed4327fec9df
LIKHITH KUMAR PAIDIMARRI <likhithpaidimarri0815@gmail.com>
2026-06-05 23:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById("getWeather").addEventListener("click", getWeather);
+function getWeather() {
+    document.getElementById("weatherData").textContent =
+        "Current weather in London: Clouds";
+}

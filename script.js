2bd876753f7edc2c0f589b14d9c0647e1a142c30
LIKHITH KUMAR PAIDIMARRI <likhithpaidimarri0815@gmail.com>
2026-06-06 00:04:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your JS code here. If required.
 document.getElementById("getWeather").addEventListener("click", getWeather);
+
 function getWeather() {
-    document.getElementById("weatherData").textContent =
-        "Current weather in London: Clouds";
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("weatherData").textContent =
+                `Current weather in London: ${data.weather[0].main}`;
+        })
+        .catch(error => console.error(error));
 }
+
+
